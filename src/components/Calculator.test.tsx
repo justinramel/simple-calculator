87904244dd40caf46e6fb32870dbb9c97a862e6d
Justin Ramel <justin.ramel@gmail.com>
2022-08-25 23:55:36 +0100

    calculator component tests

--- src/components/Calculator.test.tsx
+++ src/components/Calculator.test.tsx
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { Calculator } from "./Calculator";
 
 describe("<Calculator />", () => {
@@ -29,5 +29,84 @@ describe("<Calculator />", () => {
     expect(screen.getByRole("button", { name: "-" })).toBeDefined();
     expect(screen.getByRole("button", { name: "+" })).toBeDefined();
     expect(screen.getByRole("button", { name: "=" })).toBeDefined();
+  });
+
+  test("click buttons", async () => {
+    render(<Calculator />);
+
+    fireEvent.click(screen.getByRole("button", { name: "1" }));
+    fireEvent.click(screen.getByRole("button", { name: "2" }));
+    fireEvent.click(screen.getByRole("button", { name: "3" }));
+    fireEvent.click(screen.getByRole("button", { name: "4" }));
+    fireEvent.click(screen.getByRole("button", { name: "5" }));
+    fireEvent.click(screen.getByRole("button", { name: "6" }));
+    fireEvent.click(screen.getByRole("button", { name: "7" }));
+    fireEvent.click(screen.getByRole("button", { name: "8" }));
+    fireEvent.click(screen.getByRole("button", { name: "9" }));
+    fireEvent.click(screen.getByRole("button", { name: "0" }));
+    fireEvent.click(screen.getByRole("button", { name: "." }));
+
+    expect(screen.getByTestId("display").textContent).toBe("1234567890.");
+  });
+
+  test("add", async () => {
+    render(<Calculator />);
+
+    fireEvent.click(screen.getByRole("button", { name: "1" }));
+    fireEvent.click(screen.getByRole("button", { name: "+" }));
+    fireEvent.click(screen.getByRole("button", { name: "1" }));
+    fireEvent.click(screen.getByRole("button", { name: "=" }));
+
+    expect(screen.getByTestId("display").textContent).toBe("2");
+  });
+
+  test("subtract", async () => {
+    render(<Calculator />);
+
+    fireEvent.click(screen.getByRole("button", { name: "4" }));
+    fireEvent.click(screen.getByRole("button", { name: "3" }));
+    fireEvent.click(screen.getByRole("button", { name: "-" }));
+    fireEvent.click(screen.getByRole("button", { name: "1" }));
+    fireEvent.click(screen.getByRole("button", { name: "=" }));
+
+    expect(screen.getByTestId("display").textContent).toBe("42");
+  });
+
+  test("multiply", async () => {
+    render(<Calculator />);
+
+    fireEvent.click(screen.getByRole("button", { name: "1" }));
+    fireEvent.click(screen.getByRole("button", { name: "0" }));
+    fireEvent.click(screen.getByRole("button", { name: "x" }));
+    fireEvent.click(screen.getByRole("button", { name: "3" }));
+    fireEvent.click(screen.getByRole("button", { name: "=" }));
+
+    expect(screen.getByTestId("display").textContent).toBe("30");
+  });
+
+  test("divide", async () => {
+    render(<Calculator />);
+
+    fireEvent.click(screen.getByRole("button", { name: "2" }));
+    fireEvent.click(screen.getByRole("button", { name: "1" }));
+    fireEvent.click(screen.getByRole("button", { name: "÷" }));
+    fireEvent.click(screen.getByRole("button", { name: "3" }));
+    fireEvent.click(screen.getByRole("button", { name: "=" }));
+
+    expect(screen.getByTestId("display").textContent).toBe("7");
+  });
+
+  test("clear", async () => {
+    render(<Calculator />);
+
+    fireEvent.click(screen.getByRole("button", { name: "1" }));
+    fireEvent.click(screen.getByRole("button", { name: "2" }));
+    fireEvent.click(screen.getByRole("button", { name: "3" }));
+
+    expect(screen.getByTestId("display").textContent).toBe("123");
+
+    fireEvent.click(screen.getByRole("button", { name: "C" }));
+
+    expect(screen.getByTestId("display").textContent).toBe("0");
   });
 });
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,14 +1,23 @@
 import "./Calculator.scss";
+import { useReducer } from "react";
+import { calculatorInitialState, calculatorReducer } from "./calculatorReducer";
 
 export const Calculator = () => {
+  const [state, dispatch] = useReducer(
+    calculatorReducer,
+    calculatorInitialState
+  );
+
   return (
-    <div className="calculator" data-testid="calculator">
+    <div className="calculator">
       <label className="display" data-testid="display">
-        0
+        {state.display}
       </label>
 
       <section className="buttons">
-        <button className="clear">C</button>
+        <button className="clear" onClick={() => dispatch({ type: "CLEAR" })}>
+          C
+        </button>
         <a
           href="https://www.equalexperts.com/"
           target="_blank"
@@ -18,26 +27,76 @@ export const Calculator = () => {
           <img src="/logo.svg" className="logo" alt="Equal Experts logo" />
         </a>
 
-        <button className="operator selected">÷</button>
+        <button
+          className={`operator ${state.operator === "÷" && "selected"}`}
+          onClick={() => dispatch({ type: "PRESS_OPERATOR", operator: "÷" })}
+        >
+          ÷
+        </button>
 
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button className="operator">x</button>
+        <button onClick={() => dispatch({ type: "PRESS_OPERAND", value: "7" })}>
+          7
+        </button>
+        <button onClick={() => dispatch({ type: "PRESS_OPERAND", value: "8" })}>
+          8
+        </button>
+        <button onClick={() => dispatch({ type: "PRESS_OPERAND", value: "9" })}>
+          9
+        </button>
+        <button
+          className={`operator ${state.operator === "x" && "selected"}`}
+          onClick={() => dispatch({ type: "PRESS_OPERATOR", operator: "x" })}
+        >
+          x
+        </button>
 
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button className="operator">-</button>
+        <button onClick={() => dispatch({ type: "PRESS_OPERAND", value: "4" })}>
+          4
+        </button>
+        <button onClick={() => dispatch({ type: "PRESS_OPERAND", value: "5" })}>
+          5
+        </button>
+        <button onClick={() => dispatch({ type: "PRESS_OPERAND", value: "6" })}>
+          6
+        </button>
+        <button
+          className={`operator ${state.operator === "-" && "selected"}`}
+          onClick={() => dispatch({ type: "PRESS_OPERATOR", operator: "-" })}
+        >
+          -
+        </button>
 
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button className="operator">+</button>
+        <button onClick={() => dispatch({ type: "PRESS_OPERAND", value: "1" })}>
+          1
+        </button>
+        <button onClick={() => dispatch({ type: "PRESS_OPERAND", value: "2" })}>
+          2
+        </button>
+        <button onClick={() => dispatch({ type: "PRESS_OPERAND", value: "3" })}>
+          3
+        </button>
+        <button
+          className={`operator ${state.operator === "+" && "selected"}`}
+          onClick={() => dispatch({ type: "PRESS_OPERATOR", operator: "+" })}
+        >
+          +
+        </button>
 
-        <button className="wide">0</button>
-        <button>.</button>
-        <button className="operator">=</button>
+        <button
+          onClick={() => dispatch({ type: "PRESS_OPERAND", value: "0" })}
+          className="wide"
+        >
+          0
+        </button>
+        <button onClick={() => dispatch({ type: "PRESS_OPERAND", value: "." })}>
+          .
+        </button>
+        <button
+          className="operator"
+          onClick={() => dispatch({ type: "PRESS_EQUALS" })}
+        >
+          =
+        </button>
       </section>
     </div>
   );
